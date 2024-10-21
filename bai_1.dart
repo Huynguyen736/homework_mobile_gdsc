5fae2a0de449f50723517da3ba84bad745163a44
Huynguyen736 <nguyenhuy07032006@gmail.com>
2024-10-21 14:46:31 +0700

    bai 1

--- bai_1.dart
+++ bai_1.dart
@@ -1,5 +1,29 @@
 void main() {
-  List<int> numbers = [10,5,7,1,2,9,4,3,6,8];
-  
-  // your awnser 
+  List<int> numbers = [10, 5, 7, 1, 2, 9, 4, 3, 6, 8];
+
+  // your awnser
+  int evenBiggestNum = -1;
+  int sumOfNumbers = 0;
+  List<int> sortedNumbers = numbers.toList();
+  sortedNumbers.sort((a, b) => b.compareTo(a));
+  for (int i = 0; i < numbers.length; i++) {
+    if (sortedNumbers[i] % 2 != 0) {
+      evenBiggestNum = sortedNumbers[i];
+      break;
+    }
+  }
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i] == sortedNumbers[2]) {
+      print(i);
+    }
+    sumOfNumbers += numbers[i];
+  }
+  Iterable filterNumbers = numbers.where((number) => number > 4);
+  print(filterNumbers.toList());
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i] == evenBiggestNum) {
+      numbers[i] = sumOfNumbers;
+    }
+  }
+  print(numbers);
 }
